6c2afa070bd04b912d0cd73cf040aa0b3b6c6d72
Mathieu <mathieu.martini@edu.univ-fcomte.fr>
2021-02-02 13:16:22 +0100

    footer
    footer

--- footer.php
+++ footer.php
@@ -2,6 +2,70 @@
     <?php /** @link https://developer.wordpress.org/reference/functions/wp_footer/ */ ?>
     <?php lpwd_clean_custom_menu("footer") ?>
     <?php wp_footer(); ?>
+
+
+
+
+<footer>
+    <div class="area__footer" id="footer"> <!-- footer -->
+        <div class="row">
+            <div class="col-6 col-m-4 col-l-3"> <!-- Colonne 1 -->
+                <ul>
+                    <li>
+                        Mentions Légals
+                    </li>
+                    <li>
+                    Confidentialité 
+                    </li>
+                    <li>
+                        Conditions générales d’utilisation
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col-6 col-m-4 col-l-3 contact "> <!-- Colonne 3 -->
+                <ul>
+                    <li>
+                        Contact
+                    </li>
+                    <li>
+                        info.withings@withings.com
+                    </li>
+                    <li>
+                        +33 5 88 88 88 88
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col-6 col-m-4 col-l-3 serviceAP"> <!-- Colonne 2 -->
+                <ul>
+                    <li>
+                        Service après vente
+                    </li>
+
+                    <li>
+                        Livraison et retours 
+                    </li>
+                </ul>
+            </div>
+
+           
+            <!-- Colonne 4 -->
+            <div class="col-6 col-m-12 col-l-3 marque"><!-- Lien RS -->
+                <h2>WITHINGS</h2>
+
+
+            </div>
+        <!-- Colonne 5 -->
+            <div class="col-12"><!-- Pied -->
+            
+                <p>© 2020 Withings — Tous droits réservés.</p>
+            </div>   
+        </div> 
+
+    </div>
+</footer>
+</div>
 </body>
 
 </html>
